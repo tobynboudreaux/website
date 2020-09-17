867ffc0fdcdfd8aa45dfcb1661a2b479d4fdf27d
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-17 10:02:51 -0500

    initial commit

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,8 +3,20 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Footer</h1>
-      <p>Content</p>
+      <h1>End of Content</h1>
+      <table style={{ textAlign: "center", width: "65rem" }}>
+        <tr>
+          <th>Contact me</th>
+          <th>Other Cool Stuff</th>
+          <th>Follow</th>
+        </tr>
+        <tr>
+          <td>Link.exe</td>
+        </tr>
+        <tr>
+          <td>Link.exe</td>
+        </tr>
+      </table>
     </div>
   );
 };
