ffe51bf70847d1788fd9250e378b84418fb0f2c3
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 11:49:54 -0500

    bug squashing

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "5000";
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
